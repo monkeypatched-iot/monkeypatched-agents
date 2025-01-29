0b2587f1764b6f28673466b2388bc37c0ac4c1bf
Prashun Javeri <prashun>
2025-01-29 16:15:50 +0530

    first commit

---
+++ customer/src/utils/registry.py
@@ -0,0 +1,9 @@
+from  src.tools.functions.functions import GetCustomerDetails, GetCustomerMetadata, GetCustomerMetrics, GetCustomerPaymentInformation
+
+function_registry = {
+    "GetCustomerDetails":GetCustomerDetails,
+    "GetCustomerMetadata":GetCustomerMetadata,
+    "GetCustomerMetrics":GetCustomerMetrics,
+    "GetCustomerPaymentInformation":GetCustomerPaymentInformation
+
+}
