56448c776cde904f61399e555276ef0ebb749c0a
Prashun Javeri <prashun>
2025-02-01 12:57:01 +0530

    adding the readme

--- customer/main.py
+++ customer/main.py
@@ -9,12 +9,9 @@ def create_customer_nodes_in_knowledge_graph(customer_id: str):
         create_customer_nodes_in_knowledge_graph_helper(customer_id)
         return {"message": "Customer nodes created successfully"}
     except Exception as e:
-        # todo: clean up the orphan nodes
         print('clean up orphan nodes')
         try:
             delete_orphan_nodes()
         except Exception as e:
             print('failed to delete orphan nodes')
             raise HTTPException(status_code=500, detail=f"Error occurred: {e}")
-        
-# todo check  updates to existing customers
