63bc5a95047af8b6e0b6968b7dced743327ce04c
Prashun Javeri <prashun>
2025-02-18 13:53:15 +0530

    removing the indexer

--- documents/src/utils/registry.py
+++
@@ -1,9 +0,0 @@
-
-
-from src.functions.functions import SendDocumentUploadNotification, IndexDocument
-
-
-function_registry = {
-   "IndexDocument":IndexDocument,
-   "SendDocumentUploadNotification":SendDocumentUploadNotification
-}
